fd7c444cdabc47cf5bb525d8000d7de969683c03
usama-jamil <usamajamil006@gamil.com>
2019-06-27 04:23:09 -0700

    sending recieving data from api

--- src/Routers/user.js
+++ src/Routers/user.js
@@ -5,7 +5,6 @@ const router =new express.Router()
 
 router.post('/users', async (req,res)=>{
     const user = new User(req.body)
-
     try{
         await user.save()
         res.status(201).send(user)
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ const express = require('express')
 require('./db/mongoose')
 
 const userRouter = require('./Routers/user')
-const taskRouter = require('./Routers/task')
+const salesRouter = require('./Routers/sales')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(userRouter)
-app.use(taskRouter)
+app.use(salesRouter)
 
 app.listen(port, ()=>{
     console.log('Server is up ' + port)
