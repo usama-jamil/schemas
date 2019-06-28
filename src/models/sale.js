c67f05dd49784ccebebacafb87ea22c2cf4c5346
usama-jamil <usamajamil006@gamil.com>
2019-06-27 22:19:43 -0700

    Complete sales data

--- src/models/sale.js
+++ src/models/sale.js
@@ -1,27 +1,37 @@
-const  mongoose = require('mongoose')
-const validator = require('validator')
-
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 const saleSchema = new mongoose.Schema({
-    FullName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    ContactNumber: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    Time: { type: Date , default: new Date().toLocaleTimeString()},
-    Date:{
-        type: Date, default: Date.now
-    },
-    Status: ["CallBack", "Transfer" , "Pending"],
-    KickBack: ""    
+  FullName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  ContactNumber: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  Time: { type: Date, default: new Date().toLocaleTimeString() },
+  Date: {
+    type: Date,
+    default: Date.now
+  },
+  Status: {
+    type: String,
+    enum: ["CallBack", "Transfer", "Pending"],
+    default: "Pending"
+  },
+  KickBack: "",
+  State: String,
+  City: String,
+  ZipCode: Number,
+  Email: String,
+  MotherMediansName: String,
+  SocialSecurityNumber: String,
+  Notes: String
+});
 
-})
+const Sale = mongoose.model("Sale", saleSchema);
 
-const Sale = mongoose.model('Sale',saleSchema )
-
-module.exports = Sale
+module.exports = Sale;
