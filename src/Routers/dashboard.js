0186b242ce7d7fb527191f7d1baf14f3c9b38040
usama-jamil <usamajamil006@gamil.com>
2019-06-28 01:08:56 -0700

    Dashboard view routes

--- src/Routers/dashboard.js
+++ src/Routers/dashboard.js
@@ -1,16 +1,40 @@
-const express = require('express')
-const Sale = require('../models/sale')
-const router =new express.Router()
+const express = require("express");
+const Sale = require("../models/sale");
+const router = new express.Router();
+
+router.get("/dashboard/totalSales", async (req, res) => {
+  try {
+    const totalSales = await Sale.count();
+    res.send(totalSales);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
 
-router.get('/dashboard/totalSales',async (req,res)=>{
-
-    try{
-        const accessIps = await Sale.count()
-          res.send(accessIps)
-    } catch (e){
-        res.status(500).send(e)
+router.get("/dashboard/todaySales", async (req, res) => {
+    try {
+      const todaySales = await Sale.count({name: 'anand'});
+      res.send(todaySales);
+    } catch (e) {
+      res.status(500).send(e);
     }
+  });
 
-}
-)
+  router.get("/dashboard/callBack", async (req, res) => {
+    try {
+      const todaySales = await Sale.count({name: 'anand'});
+      res.send(todaySales);
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  });
+
+  router.get("/dashboard/transfer", async (req, res) => {
+    try {
+      const todaySales = await Sale.count({name: 'anand'});
+      res.send(todaySales);
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  });
