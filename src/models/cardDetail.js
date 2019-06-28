3d384aceb8e34ce02e936944a72aa0aec9109d7c
usama-jamil <usamajamil006@gamil.com>
2019-06-27 23:28:39 -0700

    Sales portal data

---
+++ src/models/cardDetail.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const cardDetailSchema = new mongoose.Schema({
+    CreditCardNumber : String,
+    NameOnCard : String,
+    ExpireDate: Date,
+    CVC: String,
+    BankName: String,
+    BankNumber: String,
+    Balance:Number,
+    Available: Number,
+    LastPayment: Number,
+    LastPayDate: Date,
+    DuePayment : Number,
+    DuePayDate:Date,
+    InterestRate: Number
+
+});
+
+const CardDetail = mongoose.model("CardDetail", cardDetailSchema);
+
+module.exports = CardDetail;
--- src/models/sale.js
+++ src/models/sale.js
@@ -29,7 +29,33 @@ const saleSchema = new mongoose.Schema({
   Email: String,
   MotherMediansName: String,
   SocialSecurityNumber: String,
-  Notes: String
+  Notes: String,
+
+  SecurityWord: String,
+  HighestLevelofEducation:{
+      type: String,
+      enum: ["Less than a high school diploma","High school diploma or GED","Some college or associate degree","Bachelor's Degree","Advanced/Graduate Degree"]
+  },
+  EmploymentStatus:{
+      type: String,
+      enum:["Employed Full-Time","Employed Part-Time","Self-Employed","Unemployed","Retired","Other","College Student"]
+  },
+  HousingStatus:{
+      type:String,
+      enum:["Own Home","Rent","Other"]
+  },
+  Company:String,
+  Designation:String,
+  Annualincome:Number,
+  ChequinAccounts:{
+      type:String,
+      enum:["Chequin","Saving","Chequin-Saving","None"]
+  },
+  OtherLoans:{
+      type:String,
+      enum:["Loan","Mortgages","Loan-Mortgages","Other"]
+  },
+  MonthlyRentMortgage:String
 });
 
 const Sale = mongoose.model("Sale", saleSchema);
