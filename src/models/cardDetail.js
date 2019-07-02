9cb418ce7f23b68890e44fa0a9cee341db8af0c7
usama-jamil <usamajamil006@gamil.com>
2019-07-02 00:44:59 -0700

    card Detail validation

--- src/models/cardDetail.js
+++ src/models/cardDetail.js
@@ -2,22 +2,64 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const cardDetailSchema = new mongoose.Schema({
-    CreditCardNumber : String,
-    NameOnCard : String,
-    ExpireDate: Date,
-    CVC: Number,
-    BankName: String,
-    BankNumber: String,
-    Balance:Number,
-    Available: Number,
-    LastPayment: Number,
-    LastPayDate: Date,
-    DuePayment : Number,
-    DuePayDate:Date,
-    InterestRate: Number
-
+  CreditCardNumber: {
+    type: String,
+    validate(value) {
+      if (!validator.isCreditCard(value)) {
+        throw new Error("Credit Card Number is invalid");
+      }
+    }
+  },
+  NameOnCard: {
+    type: String,
+    validate(value) {
+      if (!validator.isAlpha(value)) {
+        throw new Error("Name On Card is invalid");
+      }
+    }
+  },
+  ExpireDate: Date,
+  CVC: {
+    type: Number,
+    validate(value) {
+      if (!validator.isNumeric(value)) {
+        throw new Error("CVC is invalid");
+      }
+    }
+  },
+  BankName: {
+    type: String,
+    validate(value) {
+      if (!validator.isAlpha(value)) {
+        throw new Error("Name On Card is invalid");
+      }
+    }
+  },
+  BankNumber: {
+    type: String,
+    validate(value) {
+      if (!validator.isNumeric(value)) {
+        throw new Error("CVC is invalid");
+      }
+    }
+  },
+  Balance: { type: Number },
+  Available: { type: Number },
+  LastPayment: { type: Number },
+  LastPayDate: Date,
+  DuePayment: { type: Number },
+  DuePayDate: Date,
+  InterestRate: { type: Number }
 });
 
+
+cardDetailSchema.methods.getPublicCardDetail = function (){
+    const cardDetailObject = this.toObject()
+
+    delete cardDetailObject
+
+    return cardDetailObject
+}
 const CardDetail = mongoose.model("CardDetail", cardDetailSchema);
 
 module.exports = CardDetail;
