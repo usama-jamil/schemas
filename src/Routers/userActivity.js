1789fd7237c777a453205b53285883458c9e8a91
usama-jamil <usamajamil006@gamil.com>
2019-07-02 04:10:20 -0700

    somtimes is doo too much

--- src/Routers/userActivity.js
+++ src/Routers/userActivity.js
@@ -20,3 +20,36 @@ router.get("/userActivities", async (req, res) => {
     res.status(500).send(e);
   }
 });
+
+router.get("/userActivities/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const userActivity = await UserActivity.findById(_id);
+    console.log(userActivity);
+
+    if (!userActivity) {
+      return res.status(404).send();
+    }
+    res.status(201).send(userActivity);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+  // users.findById(_id).then users=>{
+  //     res.status(201).send users)
+  // }).catch(err =>{
+  //     res.status(500).send(err)
+  // })
+});
+
+router.delete("/userActivities/:id", async (req, res) => {
+  try {
+    const userActivity = await UserActivity.findByIdAndDelete(req.params.id);
+
+    if (!userActivity) {
+      return res.status(404).send();
+    }
+    res.send(userActivity);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
